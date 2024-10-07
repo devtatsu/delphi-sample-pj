26f1f2985cc38a48aca660badffe1469c18c96d2
OshitaTatsuya <oshita.tatsuya@tis.co.jp>
2024-10-07 13:27:34 +0900

    add db ope sample code

--- front/root.dfm
+++ front/root.dfm
@@ -11,4 +11,22 @@ object Form1: TForm1
   Font.Name = 'Segoe UI'
   Font.Style = []
   TextHeight = 15
+  object Button1: TButton
+    Left = 280
+    Top = 232
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object FDConnection1: TFDConnection
+    Left = 8
+    Top = 24
+  end
+  object FDQuery1: TFDQuery
+    Connection = FDConnection1
+    Left = 88
+    Top = 24
+  end
 end
--- front/root.pas
+++ front/root.pas
@@ -4,10 +4,20 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.VCLUI.Wait,
+  Data.DB, FireDAC.Comp.Client, FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef,
+  FireDAC.Stan.ExprFuncs, FireDAC.Phys.SQLiteWrapper.Stat, FireDAC.Stan.Param,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, FireDAC.Comp.DataSet,
+  Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    FDConnection1: TFDConnection;
+    FDQuery1: TFDQuery;
+    procedure Button1Click(Sender: TObject);
   private
     { Private 宣言 }
   public
@@ -20,5 +30,26 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+var
+  cnt: Integer;
+begin
+  // データベース接続の設定
+  FDConnection1.DriverName := 'SQLite';
+  FDConnection1.Params.Database := 'C:\workspace\delphi-sample-pj\sample_db.db';  // データベースファイルのパス
+  FDConnection1.LoginPrompt := False;
+  FDConnection1.Connected := True;
+
+  // クエリの設定
+  FDQuery1.Connection := FDConnection1;
+  FDQuery1.SQL.Text := 'SELECT count(*) as aa FROM users';  // データを取得するクエリ
+  FDQuery1.Open;  // クエリを実行してデータを取得
+
+  cnt := FDQuery1.FieldByName('aa').AsInteger;
+
+  ShowMessage(Format('Count: %d', [cnt]));
+
+end;
 
 end.
